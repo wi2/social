84415d5dc5a27b1ea31623bba63dae7d2bb44e78
Mike <contact@wi2prod.fr>
2024-02-14 14:51:44 +0100

    add post button to article

--- social-frontend/components/Articles.tsx
+++ social-frontend/components/Articles.tsx
@@ -1,9 +1,11 @@
+import { useRouter } from 'next/router';
 import { CustomLogArticleArgsType } from '../constants/type';
 import useContract from '../context/Contract';
 import { getEventSorted } from '../utils/contract';
 import Article from './Article';
 
 export default function Articles() {
+  const { query } = useRouter();
   const { articles, followedArticles } = useContract();
 
   const articlesSorted = getEventSorted<CustomLogArticleArgsType>(
@@ -12,10 +14,20 @@ export default function Articles() {
   );
 
   return (
-    <div className="flex flex-col gap-4 pt-4 pb-4">
-      {articlesSorted?.map((article) => (
-        <Article cid={article?.args._cid} key={article?.args._cid} />
-      ))}
-    </div>
+    <>
+      <div className="flex justify-end pr-4">
+        <a
+          href={`/project/posts?_slug=${query._slug}`}
+          className="btn btn-primary"
+        >
+          Add
+        </a>
+      </div>
+      <div className="flex flex-col gap-4 pt-4 pb-4">
+        {articlesSorted?.map((article) => (
+          <Article cid={article?.args._cid} key={article?.args._cid} />
+        ))}
+      </div>
+    </>
   );
 }
