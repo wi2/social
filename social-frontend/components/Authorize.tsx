58c2052c7de6f28654cff35d1f38c467d93bb8ef
Mike <contact@wi2prod.fr>
2024-02-16 00:47:22 +0100

    update copy unauthorize

--- social-frontend/components/Authorize.tsx
+++ social-frontend/components/Authorize.tsx
@@ -34,8 +34,9 @@ export default function Authorize({ children }: { children: ReactNode }) {
           <div>
             <h1 className="text-4xl font-bold">You're not Authorize.</h1>
             <p className="py-6">
-              This account user is not authorize to access to Alyra Are you sure
-              to connect with the good account
+              This account user is not authorize to access.
+              <br />
+              Are you sure to connect with the good account?
             </p>
             <a href="/" className="btn btn-primary">
               Back to homepage
