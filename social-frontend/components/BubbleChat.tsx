07c78b0889e8172ffd172698993e9ba1056ab0cd
Mike <contact@wi2prod.fr>
2024-02-15 15:38:01 +0100

    Finish bbublechat component

--- social-frontend/components/BubbleChat.tsx
+++ social-frontend/components/BubbleChat.tsx
@@ -1,21 +1,49 @@
-export default function BubbleChat({
-  user,
-  content,
-  time,
-}: {
-  user: any;
-  content: string;
-  time: any;
-}) {
+import { useEffect, useState } from 'react';
+import { Address } from 'viem';
+import { ipfsGet } from '../utils/ipfs';
+import { MessageTemplate } from '../constants/type';
+import { useAccount } from 'wagmi';
+import Avatar from './Avatar';
+import { dateFormat } from '../utils/common';
+
+export default function BubbleChat({ cid }: { cid: Address | undefined }) {
+  const { address } = useAccount();
+  const [message, setMessage] = useState<MessageTemplate>();
+
+  useEffect(() => {
+    async function main() {
+      const getContent = await ipfsGet(cid);
+      setMessage(getContent);
+    }
+    main();
+  }, [cid, setMessage]);
+
+  const isMyMessage = message?.from.address === address;
   return (
-    <div className="chat chat-start p-4">
+    <div
+      className={`chat chat-${isMyMessage ? 'end' : 'start'} p-4 grid-cols-${
+        isMyMessage ? '1' : '2'
+      } place-items-end`}
+    >
       <div className="chat-image avatar">
-        <div className="w-10 rounded-full">{user}</div>
+        <Avatar name={message?.from.address as Address} />
       </div>
-      <div className="chat-header">{user}</div>
-      <div className="chat-bubble">{content}</div>
-      <div className="chat-footer">
-        <time className="text-xs opacity-50">{time}</time>
+      <div
+        className={`chat-header justify-self-${isMyMessage ? 'end' : 'start'}`}
+      >
+        {message?.from.name}
+      </div>
+      <div
+        className={`chat-bubble justify-self-${isMyMessage ? 'end' : 'start'}`}
+      >
+        <div>{message?.content}</div>
+      </div>
+      <div
+        className={`chat-footer justify-self-${isMyMessage ? 'end' : 'start'}`}
+      >
+        <time className="text-xs opacity-50">
+          {dateFormat(message?.metadata.timestamp)?.toLocaleString()}
+        </time>
       </div>
     </div>
   );
