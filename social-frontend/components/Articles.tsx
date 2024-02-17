3c305e09df22ad580be498f32bb54b9fc6f59e0b
Mike <contact@wi2prod.fr>
2024-02-17 14:15:22 +0100

    Update message if no posts when visiting user

--- social-frontend/components/Articles.tsx
+++ social-frontend/components/Articles.tsx
@@ -3,10 +3,13 @@ import { CustomLogArticleArgsType } from '../constants/type';
 import useContract from '../context/Contract';
 import { getEventSorted } from '../utils/contract';
 import Article from './Article';
+import Link from 'next/link';
+import { displayAdress } from '../utils/common';
+import { Address } from 'viem';
 
 export default function Articles() {
   const { query } = useRouter();
-  const { allArticles, articles, followedArticles, allPins, pins } =
+  const { allArticles, articles, followedArticles, allPins, pins, profiles } =
     useContract();
 
   let articlesSorted = query._to
@@ -38,40 +41,64 @@ export default function Articles() {
   articlesSorted = articlesSorted?.filter((item) => item);
 
   if (!articlesSorted?.length) {
-    return (
-      <div className="hero">
-        <div className="hero-content flex-col lg:flex-row bg-primary-content bg-opacity-70 rounded-md">
-          <div>
-            <h1 className="text-4xl font-bold text-secondary">Welcome</h1>
-            <p className="py-6">
-              Start to add article and follows to see them here.
-            </p>
-            <span className="flex justify-end">
-              <a
-                href={`/project/posts?_slug=${query._slug}`}
-                className="btn btn-accent text-accent-content"
-              >
-                Post
-              </a>
-            </span>
+    if (!query._to) {
+      return (
+        <div className="hero">
+          <div className="hero-content flex-col lg:flex-row bg-primary-content bg-opacity-70 rounded-md">
+            <div>
+              <h1 className="text-4xl font-bold text-secondary">Welcome</h1>
+              <p className="py-6">
+                Start to add article and follows to see them here.
+              </p>
+              <span className="flex justify-end">
+                <Link
+                  href={`/project/posts?_slug=${query._slug}`}
+                  className="btn btn-accent text-accent-content"
+                >
+                  Post
+                </Link>
+              </span>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="hero">
+          <div className="hero-content flex-col lg:flex-row bg-primary-content bg-opacity-70 rounded-md">
+            <div>
+              <p className="py-6">
+                {profiles[`profile-${query._to}`] ||
+                  displayAdress(query._to as Address)}{' '}
+                has not published any articles
+              </p>
+              <span className="flex justify-around">
+                <Link
+                  href={`/project/?_slug=${query._slug}`}
+                  className="btn btn-accent text-accent-content"
+                >
+                  Go to homepage
+                </Link>
+              </span>
+            </div>
+          </div>
+        </div>
+      );
+    }
   }
 
   return (
     <>
       <div className="flex justify-end pr-4">
-        <a
+        <Link
           href={`/project/posts?_slug=${query._slug}`}
           className="btn btn-accent text-accent-content"
         >
           Add Article
-        </a>
+        </Link>
       </div>
       <div className="flex flex-col gap-4 pt-4 pb-4">
-        {articlesSorted.map((article) => (
+        {articlesSorted?.map((article) => (
           <Article cid={article?.args._cid} key={article?.args._cid} />
         ))}
       </div>
