16384191763c560157aa9367786cc48e9242aa32
Mike <contact@wi2prod.fr>
2024-03-08 08:36:28 +0100

    fix path svg loader

--- social-frontend/components/Icons.tsx
+++ social-frontend/components/Icons.tsx
@@ -230,7 +230,7 @@ export default function Icons({ icon }: { icon: string }) {
       <path
         className="opacity-75"
         fill="currentColor"
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM20"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647"
       />
     </svg>;
   }
@@ -253,7 +253,7 @@ export default function Icons({ icon }: { icon: string }) {
       <path
         className="opacity-75"
         fill="currentColor"
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647zM20"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647"
       />
     </svg>;
   }
